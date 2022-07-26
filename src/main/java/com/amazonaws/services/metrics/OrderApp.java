6cc2377e9661cc797d7aa131394c6d7c1c41f727
barry <friendship-119@163.com>
2022-07-26 21:58:38 +0800

    update

--- src/main/java/com/amazonaws/services/metrics/OrderApp.java
+++ src/main/java/com/amazonaws/services/metrics/OrderApp.java
@@ -1,17 +1,8 @@
 package com.amazonaws.services.metrics;
 
-
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.ReduceFunction;
-import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 public class OrderApp {
     StreamExecutionEnvironment env;
@@ -21,13 +12,10 @@ public class OrderApp {
     }
 
     public void run() throws Exception {
-        String[] t = new String[] {
-                "miao,She is a programmer",
-                "wu,He is a programmer",
-                "zhao,She is a programmer"
-        };
-        DataStream<String> text = env.fromElements(t);
-        text.print();
+
+        DataStream<String> text = this.env.socketTextStream("localhost", 9999, "\n", 10);
+
+        text.print("hello");
     }
 
 }
